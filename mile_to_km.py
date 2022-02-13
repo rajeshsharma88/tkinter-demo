45470a80f02cbd2c9d683d2f544dc906fc38bb7a
Rajesh <rcssharma70@gmail.com>
2022-02-13 19:38:16 +0530

    Final Commit

---
+++ mile_to_km.py
@@ -0,0 +1,28 @@
+from tkinter import *
+
+
+def miles_to_km():
+    miles = float(miles_input.get())
+    km = round(miles * 1.609)
+    km_result_label.config(text=f"{km}")
+
+
+window = Tk()
+window.title("Mile To Km Converter")
+window.config(padx=30, pady=30)
+
+miles_input = Entry(width=7)
+miles_input.grid(column=1, row=0)
+miles_label = Label(text="Miles")
+miles_label.grid(column=2, row=0)
+
+is_equal_label = Label(text="is equal to")
+is_equal_label.grid(column=0, row=1)
+km_result_label = Label(text="0")
+km_result_label.grid(column=1, row=1)
+km_label = Label(text="km")
+km_label.grid(column=2, row=1)
+calculate_btn = Button(text="Calculate", command=miles_to_km)
+calculate_btn.grid(column=1, row=2)
+
+window.mainloop()
